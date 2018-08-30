fa5d6729b5eb1c2dda1a8242a8ef84f8a4fa7f7a
罗怡 <luoyi06@baidu.com>
2018-08-30 10:48:54 +0800

    导出图片组件话

--- src/html2canvas.js
+++ src/html2canvas.js
@@ -1,30 +1,47 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import html2canvas from 'html2canvas';
-import './index.css';
-// import domtoimage from 'dom-to-image';
-// import {Button} from 'mui-react'
-// import mui from 'mui';
-// import './html2canvas.css';
+// import './index.css';
 
-function ClickToImage(props) {
-    return <button onClick={props.clickToImage}>点我生成图片</button>
-}
+// function ClickToImage(props) {
+//     return <div onClick={props.clickToImage}>{props.children}</div>
+// }
 
 class Index extends React.Component {
+    static propTypes = {
+        clickBtn: PropTypes.string,
+        executeArea: PropTypes.string,
+        renderLayout: PropTypes.func,
+        // pref: PropTypes.element
+    }
+    static defaultProps = {
+        clickBtn: '',   // 点击元素的className
+        executeArea: '',    // 生成图片的元素的className
+        renderLayout: () => null,   // 渲染函数
+        // pref: null
+    }
     constructor(props) {
         super(props);
         this.state = {isDownloadImg:false};
         this.toggleButton = this.toggleButton.bind(this);
         // this.longPressSave = this.longPressSave.bind(this);
     }
-    toggleButton() {
+    toggleButton(e) {
+        if (this.props.clickBtn && !e.target.className.includes(this.props.clickBtn)) {
+            return false;
+        }
+        console.log(e.target.className);
+        console.log(this.props.executeArea);
+        console.log(document.getElementsByClassName(this.props.executeArea));
         let self = this;
-        let card_target = this.refs.copyCardArea; 
+        // let card_target = this.refs.copyCardArea; 
+        let card_target = document.getElementsByClassName(this.props.executeArea)[0];
         // domtoimage组件中，IOS不能截图，并且污染了canvas.toDataUrl会报错，目前没有解决方案
-        // 所以目前暂不使用domtoimage  
+        // 所以目前暂不使用domtoimage 
         let b64;
-        let width = this.refs.copyCardArea.scrollWidth;
-        let height = this.refs.copyCardArea.scrollHeight;
+        let height = document.documentElement.clientHeight;
+        let width = card_target.clientWidth * (document.documentElement.clientHeight/card_target.scrollHeight);
+        console.log(width,height);
         html2canvas(card_target, {
             useCORS: true
         }).then(function(canvas) {
@@ -42,21 +59,28 @@ class Index extends React.Component {
         }).catch(function onRejected(error) {});
     }
     render() {
+        const {renderLayout} = this.props;
         return (
             <div>
-                <ClickToImage className="save-button" clickToImage={this.toggleButton} />
-                {/* <button className="save-button"  onClick={()=>this.cropCard()}>点击保存名片</button>  */}
-                <div className="company-card-info center-father-style-align" ref="copyCardArea" id="copyCardArea">
-                    <div id="copyArea" className="copyArea" ref="copyArea">
-                        {this.props.children}
-                        {/* <img src={this.state.url} /> */}
-                    </div>
+                <div
+                    className="save-button" 
+                    onClick={(e) => this.toggleButton(e)}
+                    style={{display: this.state.isDownloadImg ? "none":"block"}}
+                >
+                    {renderLayout()}
                 </div>
                 
-                <h4>下面是截图部分：</h4>
-                
-                <div className="img-show"  style={{display: this.state.isDownloadImg ? "block":"none"}}>
-                    <img src={this.state.imgUrl} style={{width:this.state.width, height:this.state.height}}/>
+                <div 
+                    className="img-show"
+                    style={{display: this.state.isDownloadImg ? "block":"none"}}
+                >
+                    <img 
+                        src={this.state.imgUrl} 
+                        style={{
+                            width:this.state.width, 
+                            height:this.state.height
+                        }}
+                    />
                 </div>
             </div>
         )
--- src/index.js
+++ src/index.js
@@ -11,12 +11,24 @@ import Caculator from './caculator';
 import Dialog from './dialog';
 // import HelloMessage from './webComponents';
 import Theme from './theme';
+import { relative, isAbsolute } from 'path';
 
 class Import extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {className: "screenArea"};
+        // this.allbody = React.createRef();
+    }
+    renderLayout() {
+        return <button className="html2image">点我生成图片</button>
+    }
     render() {
+        const className = this.state.className;
         return (
-            <Index>
-                <Theme />
+            <div className="_namespace"  style={{position: relative}}>
+            {/* ref = {allbody => this.allbody = allbody} */}
+                <div className={className}>
+                    <Theme />
                     <App />
                     {/* <HelloMessage /> */}
                     <Toggle />
@@ -27,16 +39,23 @@ class Import extends React.Component {
                     <div className="company-name-short">罗怡</div>
                     <div className="company-name-full">百家号</div>
                     <div className="company-connection">
-                    <div className="company-phone">
-                        <div className="inline-block company-phone-text">电话：</div>
-                        <div className="inline-block company-phone-number">*保密*</div>
+                        <div className="company-phone">
+                            <div className="inline-block company-phone-text">电话：</div>
+                            <div className="inline-block company-phone-number">*保密*</div>
+                        </div>
+                        <div>
+                            <div className="inline-block company-webmail-text">邮箱：</div>
+                            <div className="inline-block company-webmail-number">luoyi06@baidu.com</div>
+                        </div>
                     </div>
-                    <div>
-                        <div className="inline-block company-webmail-text">邮箱：</div>
-                        <div className="inline-block company-webmail-number">luoyi06@baidu.com</div>
-                    </div>
-                    </div>
-            </Index>
+                </div>
+                <Index
+                    clickBtn= "html2image"
+                    executeArea= {className}
+                    renderLayout= {this.renderLayout.bind(this)}
+                    style={{position: isAbsolute, left: 0, top:0}}
+                />
+            </div>
         )
     }
 }
